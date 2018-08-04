fd85c261a00e364341bf958e5c3c7d949b307f3b
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-04 00:16:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -11,12 +11,13 @@ end
 =end
 
 def is_prime(x):
-    if x < 2:
-        return false
-    else:
-        for n in range(2,x):
-            if x % n == 0:
-               return false
-        return true
+  if x < 2
+      return false
+  else
+      for n in range(2,x):
+          if x % n == 0:
+              return false
+      return true
     end
+  end
 end
