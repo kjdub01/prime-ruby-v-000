fa0b57c603fad3f923af79e3a6204371d24182f4
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-04 00:14:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 require 'pry'
+=begin
 def prime?(i)
  divisors = (2..(i - 1)).to_a  
  divisors.any? do |divisor|
@@ -7,4 +8,13 @@ def prime?(i)
  end
  true
 end
+=end
 
+def is_prime(x):
+    if x < 2:
+        return false
+    else:
+        for n in range(2,x):
+            if x % n == 0:
+               return false
+        return true
