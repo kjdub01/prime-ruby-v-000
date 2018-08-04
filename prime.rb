012a3dcd60b2c6e0406b7929d8b4cb4d23e7187d
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-04 00:08:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,7 @@ require 'pry'
 def prime?(i)
  divisors = (2..(i - 1)).to_a  
  divisors.any? do |divisor|
-   if i <= 1 || i % divisor == 0
-     return false
+  return false if i <= 1 || i % divisor == 0
   end
  end
 end
